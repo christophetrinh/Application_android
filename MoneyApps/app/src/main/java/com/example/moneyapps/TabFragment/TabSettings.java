8846b4bb11bd3726466d5d68989498c1c8686637
Mario Viola <mario.viola@epfl.ch>
2016-12-08 15:52:58 +0100

    Change title colour

--- MoneyApps/app/src/main/java/com/example/moneyapps/TabFragment/TabSettings.java
+++ MoneyApps/app/src/main/java/com/example/moneyapps/TabFragment/TabSettings.java
@@ -1,6 +1,10 @@
 package com.example.moneyapps.TabFragment;
 
+import android.content.SharedPreferences;
+import android.icu.text.SymbolTable;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +17,14 @@ import com.example.moneyapps.R;
  * Created by mario on 29/11/2016.
  */
 
-public class TabSettings extends PreferenceFragment {
+public class TabSettings extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public String date_preference_ToLoad;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+        View v =  inflater.inflate(R.layout.settings_fragment, container, false);
+        return v;
     }
 
     @Override
@@ -24,9 +32,26 @@ public class TabSettings extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+/*
+        ListPreference date_preference = (ListPreference) findPreference("date_preference");
+        date_preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                date_preference_ToLoad = newValue.toString();
+                System.out.println(date_preference_ToLoad);
+                return true;
+            }
+        });*/
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
+        if (key.equals("date_preference")) {
+            //do something
+            System.out.println(key);
+        }
 
+    }
 
 }
